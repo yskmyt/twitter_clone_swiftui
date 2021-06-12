fc11eb6a5ab69921438fff9d7eeeb94aa3517828
Yusuke Miyata <yskmyt.dev@gmail.com>
2021-06-12 18:09:50 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Yusuke Miyata on 2021/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/twitter_clone_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_clone_swiftuiApp.swift
+//  Shared
+//
+//  Created by Yusuke Miyata on 2021/06/12.
+//
+
+import SwiftUI
+
+@main
+struct twitter_clone_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
